bcd3eb9aead6a00f718939e5cab3ed6a407c0083
df1r <78988163+df1r@users.noreply.github.com>
2025-02-14 13:31:07 -0500

    Update files by upload
    Generate text by getting a random joke from the jokes api
    Rewrite instructions to be clearer
    Repair some CSS issues and some focus issues
    Deal with strings that contain new-lines and quotation marks
    Deal with accidental strikes on F1 through F12

--- index.js
+++ index.js
@@ -7,9 +7,35 @@ const port = 3000;
 app.use(express.static("public"));
 
 app.get("/", (req, res)=> {
-    res.render("index.ejs", {text: "hello, world"});
+    var renderHeaders ={
+        "textFromIndex": "[Text will appear after Start button is clicked.]",
+        "runningCode": false,
+    }
+    res.render("index.ejs", {"passedObject": renderHeaders});
+});
+
+app.get("/typing", async (req, res)=> {
+    try {
+        let result = await axios.get('https://v2.jokeapi.dev/joke/Any?type=single');
+        var renderHeaders = {
+            "textFromIndex": result.data.joke.replaceAll("/n","<br/>"),
+            "runningCode": true,
+        };
+        res.render("index.ejs", {"passedObject": renderHeaders});
+    } catch (error) {
+        console.log(`caught an error: ${error}`);
+        var renderHeaders = {
+            "textFromIndex": error,
+            "runningCode": false,
+        };
+        res.render("index.ejs", {"passedObject": renderHeaders});
+    }
 });
 
 app.listen(port, ()=> {
     console.log(`Server is running on port ${port}`);
 });
+
+//5) When I render from the "/typing" endpoint the URL field says "/typing?".
+//6) Can I deal with the dash and the umlaut? I think I will replaceAll in the target text and turn the
+//characters with accents into plain characters and turn the dash into a double-hyphen.
--- public/keyshift.js
+++ public/keyshift.js
@@ -6,11 +6,6 @@ The constant "strike" defines the mapping of each key to another key.
 Note: there is something weird about the ampersand. It is impossible to delete
 a string of text that ends with an "&". I don't yet know why.
 
-Note: After a button click, a space-button press acts as a re-click of the button.
-I have to learn how to disable this, because instead of the text freezing when the
-timer runs out, the typist will hit the next space bar and will erase the text.
-For now I will put a 5-second delay on the end of the count-down and the re-activation
-of the button.
 */
 const strike = {//This is an array giving the correspondence of the keys, effectively shifting the position of the hands over the keyboard.
     1: "q", 2: "w", 3: "e",4:"r",5:"t",6:"y",7:"u",8:"i",9:"o",0:"p",'-':"[",'=':"]",'\\':"<br>",
@@ -23,24 +18,22 @@ const strike = {//This is an array giving the correspondence of the keys, effect
     Z:"!",X:"@",C:"#",V:"$",B:"%",N:"^",M:" ",'<':"*",'>':"(",'?':")",
     ArrowLeft:"-",ArrowRight:"=",ArrowUp:"+",ArrowDown:"_"
 };
-const startCount=200;//Modify this line to make the time constraint longer or shorter, in seconds.
+const startCount=10+Math.floor($("#goal-text").text().length*0.5);
 var countDownValue=startCount;//initialize the count-down at page load
-var pageNumber;
 $(".count-down").html(startCount);//display the count-down
 $("#start-value").text(startCount);//insert the time constraint into the instructions
-$("#goal-text").text("[Text will appear after Start button is clicked.]");
-$("#start-button").on("click", onClick );//set an event listener for the button
-function onClick() {//call-back function when button is pressed
-    pageNumber=Math.floor(100+20*(Math.random()));
-    $("#goal-text").text($("#n"+pageNumber).text());
+
+
+if(isTyping) {
     countDownValue=startCount;//re-start the clock
     $(".count-down").html(startCount);//display the count-down
-    $("#start-button").unbind();//de-activate the button
     $("#start-button").addClass("pressed-button");//make the button look de-activated
+    $("#start-button").attr('disabled','disabled');//deactivate the button
     $("#modify-text").html("_");//add a pseudo-cursor
     $("#modify-text").removeClass("green-background");//clear the state that says the user has won
     $("#modify-text").removeClass("red-background");//clear the state that says the user has lost
     $(document).keydown( function(e)  {//set an event listener for the keyboard strikes with embedded anonymous call-back function
+        e.preventDefault(); //this is to stop the space-bar or tab key from refocusing and scrolling away
         switch (e.key) {//this switch handles the correspondence between the pressed keys and the resulting characters
             case "Backspace": //In this case, delete the latest character typed
                 if ($("#modify-text").html().slice(-5,-1) === "<br>") {//we handle the backspace over a new-line separately, since we need to delete four characters from the string, <br>.
@@ -53,17 +46,28 @@ function onClick() {//call-back function when button is pressed
             break;
             case "Enter": $("#modify-text").html($("#modify-text").html().slice(0,-1)+"<br>"+"_");
             break;
-            /* Tab takes the focus out of the window. I can't de-activate that behavior,
-            so I won't try to deal with the Tab case.
-            case "Tab": $("#modify-text").html($("#modify-text").html()+"    ");
-            break;*/
+            case "Tab": $("#modify-text").html($("#modify-text").html().slice(0,-1)+"    "+"_");
+            break;
             case "Shift"://all of these cases describe key-presses that have no effect, essentially de-activating those keys.
+            case "F1":
+            case "F2":
+            case "F3":
+            case "F4":
+            case "F5":
+            case "F6":
+            case "F7":
+            case "F8":
+            case "F9":
+            case "F10":
+            case "F11":
+            case "F12":
             case "CapsLock":
             case "Delete":
             case "Control":
             case "Escape":
             case "Meta":
             case "Unidentified":
+            case "undefined":
             case "AudioVolumeMute":
             case "AudioVolumeDown":
             case "AudioVolumeUp":
@@ -73,32 +77,31 @@ function onClick() {//call-back function when button is pressed
             break;
             default:  $("#modify-text").html($("#modify-text").html().slice(0,-1)+strike[e.key]+"_");//remove cursor, add new character, add cursor 
         }
-        if ($("#modify-text").text().slice(0,-1) === $("#goal-text").text()) {//check whether the user won, and if so, de-activate the keyboard, stop the timer, re-activate the button after a five-second delay, and make the box green.
+        if ($("#modify-text").text().slice(0,-1) === $("#goal-text").text().replaceAll("\n","")) {//check whether the user succeeded, and if so, remove the pseudo-cursor, de-activate the keyboard, stop the timer, re-activate the button after a five-second delay, and make the box green.
+            $("#modify-text").text($("#goal-text").text());
             $(document).unbind();
-            clearInterval(x);
+            clearInterval(x);  //x is the setInterval value for the count-down--see code just below the end of this call-back.
             setTimeout(function() {
-                $("#start-button").on("click", onClick);
                 $("#start-button").removeClass("pressed-button");
-            },5000);
+                $("#start-button").removeAttr('disabled');//reactivate the button
+            },2000);
             $("#modify-text").addClass("green-background");
         } 
-    });//end of keyboard event-listener call-back function. We are still inside the click call-back function
+    });//end of keyboard event-listener call-back function.
+    countDownValue++;
     var x=setInterval(function() {//start the timer
         countDownValue--;
         $(".count-down").html(countDownValue);
-        if ( countDownValue <= 0 ) {//check whether the user lost, and if so, stop the count-down, de-activate the keyboard, make the box red, and after a five-second delay, re-activate the button
+        if ( countDownValue <= 0 ) {//check whether the user failed, and if so, stop the count-down, de-activate the keyboard, make the box red, and after a five-second delay, re-activate the button
             clearInterval(x);
             $(document).unbind();
             $("#modify-text").addClass("red-background");
             setTimeout(function() {
                 $(".count-down").html(countDownValue);
-                $("#start-button").on("click", onClick);
                 $("#start-button").removeClass("pressed-button");
-            },5000);
+                $("#start-button").removeAttr('disabled');//reactivate the button
+            },2000);
         }
     },1000);
 }
-
-//Grab the text to be typed randomly from the jokes API.
 //why can't I backspace past the ampersand?
-//Learn how to change the "focus?" so that the space key doesn't re-click the button.
